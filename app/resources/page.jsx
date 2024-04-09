b25c4970aed8e59fd4fa61d44797e7935526c01a
midCenturyDude7 <mgriffes@gmail.com>
2024-04-08 21:20:09 -0700

    Implemented routes

---
+++ app/resources/page.jsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 text-slate-200">
+      This is the resources page.
+    </div>
+  );
+}
