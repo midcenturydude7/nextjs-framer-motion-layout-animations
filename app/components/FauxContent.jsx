c1a8de0e8d29c9b23415aea93a796c710b0180b1
midCenturyDude7 <mgriffes@gmail.com>
2024-04-17 19:10:06 -0700

    Updated styles

--- app/components/FauxContent.jsx
+++ app/components/FauxContent.jsx
@@ -9,10 +9,11 @@ export default function FauxContent() {
 
   return (
     <main className="px-8 pt-44">
-      <h1 className="mb-8 text-xl text-slate-200/80">
-        &#187;{" "}
-        <span className="font-bold text-lime-300/90">{currentPage} Page</span>{" "}
-        &#171;
+      <h1 className="mb-8 cursor-pointer text-xl">
+        <span className="text-2xl text-stone-200/50">&#187;</span>{" "}
+        <span className="font-bold text-lime-300/90 hover:text-lime-300">
+          {currentPage} Page
+        </span>
       </h1>
       <h2 className="h-10 w-4/5 rounded bg-slate-700/25 text-2xl font-bold" />
       <div className="mt-8 space-y-8">
