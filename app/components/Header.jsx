6922944c8cee10ddb91ba38f2016eb6ef89d07de
midCenturyDude7 <mgriffes@gmail.com>
2024-04-17 21:29:34 -0700

    Implemented scroll animation

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,16 +1,79 @@
 "use client";
 import React from "react";
 import Menu from "./Menu";
+import {
+  motion,
+  useScroll,
+  useMotionValue,
+  useTransform,
+  useMotionTemplate,
+} from "framer-motion";
+
+function clamp(number, min, max) {
+  return Math.min(Math.max(number, min), max);
+}
+
+function useBoundedScroll(bounds) {
+  let { scrollY } = useScroll();
+  let scrollYBounded = useMotionValue(0);
+  let scrollYBoundedProgress = useTransform(
+    scrollYBounded,
+    [0, bounds],
+    [0, 1],
+  );
+
+  React.useEffect(() => {
+    return scrollY.on("change", (current) => {
+      let previous = scrollY.getPrevious();
+      let diff = current - previous;
+      let newScrollYBounded = scrollYBounded.get() + diff;
+
+      scrollYBounded.set(clamp(newScrollYBounded, 0, bounds));
+    });
+  }, [scrollY, scrollYBounded, bounds]);
+
+  return { scrollYBounded, scrollYBoundedProgress };
+}
 
 export default function Header() {
+  let { scrollYBoundedProgress } = useBoundedScroll(150);
+  let scrollYBoundedProgressThrottled = useTransform(
+    scrollYBoundedProgress,
+    [0, 0.75, 1],
+    [0, 0, 1],
+  );
+
   return (
     <div className="mx-auto flex w-full max-w-3xl flex-1 overflow-hidden text-slate-600">
       <div className="z-0 flex-1">
-        <header className="fixed inset-x-0 flex items-center justify-between border-b-[1px] border-[#3f3c3c]/80 bg-[#222731]/60 py-10 shadow backdrop-blur-md px-10">
-          <div>LOGO</div>
+        <motion.header
+          className="fixed inset-x-0 flex items-center justify-between border-b-[1px] border-[#3f3c3c]/80 bg-[#222731]/60 px-10 py-10 shadow backdrop-blur-md"
+          style={{
+            height: useTransform(
+              scrollYBoundedProgressThrottled,
+              [0, 1],
+              [150, 75],
+            ),
+            backgroundColor: useMotionTemplate`rgb(34 39 49 / ${useTransform(scrollYBoundedProgressThrottled, [0, 1], [1, 0.1])}`,
+          }}
+          animate={{ ease: "easeInOut", duration: 1.5 }}
+        >
+          <motion.div
+            className="text-3xl"
+            style={{
+              scale: useTransform(
+                scrollYBoundedProgressThrottled,
+                [0, 1],
+                [1, 0.9],
+              ),
+            }}
+            animate={{ ease: "easeInOut", duration: 1.5}}
+          >
+            LOGO
+          </motion.div>
           <Menu />
           <div>ICONS</div>
-        </header>
+        </motion.header>
       </div>
     </div>
   );
--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -3,8 +3,34 @@ import React from "react";
 import { navItems } from "../lib/navItems";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { motion, useTransform, useScroll, useMotionValue } from "framer-motion";
 import "../scene.css";
+
+function clamp(number, min, max) {
+  return Math.min(Math.max(number, min), max);
+}
+
+function useBoundedScroll(bounds) {
+  let { scrollY } = useScroll();
+  let scrollYBounded = useMotionValue(0);
+  let scrollYBoundedProgress = useTransform(
+    scrollYBounded,
+    [0, bounds],
+    [0, 1],
+  );
+
+  React.useEffect(() => {
+    return scrollY.on("change", (current) => {
+      let previous = scrollY.getPrevious();
+      let diff = current - previous;
+      let newScrollYBounded = scrollYBounded.get() + diff;
+
+      scrollYBounded.set(clamp(newScrollYBounded, 0, bounds));
+    });
+  }, [scrollY, scrollYBounded, bounds]);
+
+  return { scrollYBounded, scrollYBoundedProgress };
+}
 
 export default function Menu() {
   const pathname = usePathname();
@@ -12,10 +38,20 @@ export default function Menu() {
   const [focused, setFocused] = React.useState(null);
   const [selected, setSelected] = React.useState(pathname);
 
+  let { scrollYBoundedProgress } = useBoundedScroll(150);
+  let scrollYBoundedProgressThrottled = useTransform(
+    scrollYBoundedProgress,
+    [0, 0.75, 1],
+    [0, 0, 1],
+  );
+
   return (
-    <nav
+    <motion.nav
       onMouseLeave={() => setFocused(null)}
       className="flex w-[fit-content] items-center justify-center gap-8 rounded-[8px] border-[1px] border-[#1A1D23] border-[solid] bg-[#1A1D23] py-[.65rem] pl-[1.35rem] pr-[2rem]"
+      style={{
+        opacity: useTransform(scrollYBoundedProgressThrottled, [0, 1], [1, 0]),
+      }}
     >
       {navItems.map(({ path, id, label }) => {
         const isActive = path === pathname;
@@ -58,6 +94,6 @@ export default function Menu() {
           </Link>
         );
       })}
-    </nav>
+    </motion.nav>
   );
 }
