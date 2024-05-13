f529f92b83738a00c81685e33fc49a744ef9608d
midCenturyDude7 <mgriffes@gmail.com>
2024-05-12 21:16:54 -0700

    Working on setting up skeleton display of card

--- app/resources/components/StretchCard.jsx
+++ app/resources/components/StretchCard.jsx
@@ -7,9 +7,79 @@ import styles from "./StretchCard.module.css";
 
 export default function StretchCard({ card, setActiveCard }) {
   return (
-    <div className={styles.card}>
+    <div
+      onClick={() => setActiveCard(card)}
+      className={styles.card}
+      style={{ borderRadius: 20 }}
+    >
       <img src={card.image} alt="image" style={{ borderRadius: 20 }} />
-      <button aria-hidden tabIndex={-1} className={styles.closeButton}></button>
+      <button aria-hidden tabIndex={-1} className={styles.closeButton}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          height="20"
+          width="20"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18 18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
+      <div className={styles.cardContent}>
+        <div className={styles.cardText}>
+          <h2 className={styles.cardHeading}>Game of the day</h2>
+        </div>
+        <div
+          className={styles.extraInfo}
+          style={{ borderBottomLeftRadius: 20, borderBottomRightRadius: 20 }}
+        >
+          <img
+            src={card.logo}
+            width={40}
+            height={40}
+            alt="play"
+            className="rounded-lg"
+          />
+          <div className={styles.descWrapper}>
+            <span className={styles.gameTitle}>{card.title}</span>
+            <span className={styles.gameDescription}>{card.description}</span>
+          </div>
+          <button className={styles.getButton}>Get</button>
+        </div>
+      </div>
+
+      <div
+        className={styles.longDescription}
+        style={{ position: "absolute", top: "100%", opacity: 0 }}
+      >
+        <div>
+          <p>
+            <b>Are you ready?</b> {card.longDescription}
+          </p>
+          <p>
+            <b>The never ending adventure</b>
+            In this game, set in a fairy tale world, players embark on a quest
+            through mystical lands filled with enchanting forests and towering
+            mountains.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ActiveCard({ activeCard, setActiveCard }) {
+  const ref = React.useRef(null);
+  useConClickOutside(ref, () => setActiveCard(null));
+
+  return (
+    <div className={`${styles.card} ${styles.cardActive}`}>
+      <div className={styles.cardInner}></div>
     </div>
   );
 }
